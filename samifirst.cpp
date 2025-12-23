cb6abea41f10682f9b4c8c3dc4c326a3a5c76fc0
samizzz67 <abiyafikre@gmail.com>
2025-12-23 11:11:17 +0300

    sami first

---
+++ samifirst.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"Enter the numbers here:_"<<endl;
+int num[2][3],i,j;
+for(int i=0; i<2; i++){
+   for (int j=0; j<3; j++){
+    cin>>num[i][j];
+
+   } 
+}
+for (int i =0 ;i<2; i++){
+    for (int j=0; j<3; j++){
+cout<<num [i][j]<<"  ";
+    }
+    cout<<endl;
+}
+
+}
