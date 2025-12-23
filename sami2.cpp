a7e8b49e0a0150d6e3d07ca5df8cee77119c48d6
samizzz67 <abiyafikre@gmail.com>
2025-12-23 11:19:52 +0300

    thsi is

---
+++ sami2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    int num[3],nu[3],i;
+    nu[3]=(0,0,0);
+    //,num1[5];
+    //num[5]=(1,2,6,5,80);
+    //num1[5]=(1,3,8,54,6);
+    //cout<<max(3,4);
+    for(int i=0; i<3; i++){
+        cin>>num[i];
+       
+    }
+     cout<<max(num[i],nu[3]);
+   //cout<<max(num1[5],num[5]);
+}
